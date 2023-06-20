aed87302d4a23c7b0720b0dcd146cb2e63883132
0xMere <merenda.s.mattia@gmail.com>
2023-06-20 23:06:21 +0200

    General commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # my-gpt4
+
+> Credits: https://github.com/xtekky/gpt4free/tree/main/gpt4free
+
+## Installation
+```
+pip install gpt4free
+```
+```
+git clone https://github.com/merendamattia/my-gpt4.git
+```
+
+## Execution
+```
+python3 gpt-4.py
+```
+
+---
+
+### TODO list
+- Possibilità di pulire la chat
+- Possibilità di creare più chat diverse (un file di testo per ogni chat)
---
+++ gpt-4.py
@@ -0,0 +1,63 @@
+# Importo GPT-4
+from gpt4free import you
+
+# Serve per stampare le scritte colorate
+from colorama import init, Fore, Back, Style
+init()
+
+# Vado a verificare se il file history_chat esiste già
+# Se non esiste lo creo
+import os
+if not os.path.isfile('history_chat.txt'):
+    with open('history_chat.txt', 'w') as file:
+        file.write("My history-chat$$$by @merendamattia")
+
+# Messaggio iniziale
+print(Fore.RED + "#chatbot model GPT-4 (`q`: to quit)" + Fore.RESET)
+
+chat = []
+
+# Mi serve per aggiornare poi la history-chat
+output = "" 
+
+# Leggo la history-chat e 're-insegno' le cose a gpt
+with open('history_chat.txt', 'r') as file:
+    for riga in file:
+        contenuto_riga = riga.split("$$$")
+        question = contenuto_riga[0].strip()
+        answer = contenuto_riga[1].strip()
+
+        chat.append({"question": question, "answer": answer})
+
+# Esecuzione iterazione GPT-4
+while True:
+    # Leggo la domanda in input
+    prompt = input(Fore.GREEN + "You: " + Fore.RESET)
+    
+    # Serve per uscire dalla chat
+    if prompt == 'q':
+        break
+
+    # Calcolo la risposta
+    response = you.Completion.create(
+        prompt=prompt,
+        chat=chat,
+        detailed=True)
+
+    # Stampo la risposta
+    print(Fore.BLUE + "Bot:" + Fore.RESET, response.text)
+
+    # Inserisco domanda e risposta nella history-chat
+    chat.append({"question": prompt, "answer": response.text})
+    # Inserisco domanda e risposta nella history-chat da scrivere poi sul file
+    output += prompt + "$$$" + response.text + "\n"
+
+
+# Leggo il vecchio contenuto della history-chat
+old = ""
+with open('history_chat.txt', 'r') as file:
+    old = file.read()
+
+# Scrivo la nuova history-chat aggiornata
+with open('history_chat.txt', 'w') as file:
+    file.write(old + output)
